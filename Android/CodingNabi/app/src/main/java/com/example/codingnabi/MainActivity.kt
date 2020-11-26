7c1fecccf96459c42492fca15ad1d86d28a244f4
BearHunter49 <rkdcjf987@naver.com>
2020-11-27 00:14:54 +0900

    Logo changed & added splash

--- Android/CodingNabi/app/src/main/java/com/example/codingnabi/MainActivity.kt
+++ Android/CodingNabi/app/src/main/java/com/example/codingnabi/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_CodingNabi)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }

621db76f6413ab2183e701f662fea5b588a67e5d
BearHunter49 <rkdcjf987@naver.com>
2020-11-27 16:41:06 +0900

    Set basic UI.

--- Android/CodingNabi/app/src/main/java/com/example/codingnabi/home/HomeFragment.kt
+++ Android/CodingNabi/app/src/main/java/com/example/codingnabi/home/HomeFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.codingnabi.R
+import com.example.codingnabi.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
 }
--- Android/CodingNabi/app/src/main/java/com/example/codingnabi/home/MainActivity.kt
+++ Android/CodingNabi/app/src/main/java/com/example/codingnabi/home/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.codingnabi.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.codingnabi.R
+import com.example.codingnabi.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_CodingNabi)
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         Timber.i("App Start!")
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+
+        binding.homeBottomNavigation.setupWithNavController(navHost.navController)
+
     }
 }
